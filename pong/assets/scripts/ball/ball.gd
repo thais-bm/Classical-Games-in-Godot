d4d5c6b3dae14bee78450a5305be3fbd1468f19c
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2025-01-31 14:23:35 -0300

    Bug Fix: Bounce SFX is looping

--- pong/assets/scripts/ball/ball.gd
+++ pong/assets/scripts/ball/ball.gd
@@ -1,8 +1,6 @@
 extends RigidBody2D
 
 var base_speed: int = 300
-signal collided
-var has_played
 
 # A bola do jogo Pong
 # Escala da gravidade -> ZERO
@@ -14,9 +12,6 @@ func _ready() -> void:
 	linear_velocity = direction * base_speed
 
 
-func _on_body_entered(body: Node) -> void:
-	if not has_played:
-		print("aqui entra")
-		var bounce: AudioStreamPlayer = get_node("Bounce")
-		bounce.play()
-		has_played = true
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is StaticBody2D or body is CharacterBody2D:
+		get_node("Bounce").play()
